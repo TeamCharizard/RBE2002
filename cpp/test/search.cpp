f7849350dd285997bc56ae5eafd5836b3610b9d7
Alan turing <ed@i.t>
2015-12-04 18:52:41 +0000

    starting search. doesn't tun enough

---
+++ cpp/test/search.cpp
@@ -0,0 +1,57 @@
+#include "DriveMotor.hpp"
+#include "Lidar.hpp"
+#include <stdio.h>
+#include <signal.h>
+
+Lidar lidar;
+DriveMotor left(6);
+DriveMotor right(5);
+
+void my_handler(int s){
+  left.set(0);
+  right.set(0);
+  exit(1);
+}
+
+int main(int argc, char **argv){
+  struct sigaction sigIntHandler;
+
+  sigIntHandler.sa_handler = my_handler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+
+  sigaction(SIGINT, &sigIntHandler, NULL);
+
+  int *distances;
+
+  while (true) {
+    bool fullSweep = lidar.read();
+
+    if (fullSweep){
+      distances = lidar.distances;
+      int dFront = distances[0],
+          dRight = distances[10],
+          dLeft = distances[350];
+
+      printf("dFront=%d dRight=%d dLeft=%d\n",dFront,dRight,dLeft);
+
+      if (dFront < 400 && dFront > 0){
+        if (dRight > dLeft){
+          //turn right
+          left.set(100);
+          right.set(0);
+        }
+        else {
+          //turn left
+          left.set(0);
+          right.set(100);
+        }
+      }
+      else {
+        left.set(-50);
+        right.set(50);
+
+      }
+    }
+  }
+}
