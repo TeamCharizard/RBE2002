a6db0f9c483872d8644f08165e79e3bc3cc4e4af
Peter_Mitrano <mitranopeter@gmail.com>
2015-12-11 00:59:39 -0500

    docs stuff

---
+++ src/Docs.h
@@ -0,0 +1,15 @@
+/**
+ * \mainpage Woah, doxygen is super cool!
+ *
+ * Here are the dank kids who made this:
+ * \Author <a href = "https://petermitrano.github.io">Peter Mitrano</a>,
+ * \Author Joseph Martin,
+ * \Author Travis Norris
+ *
+ * Pictures and Videos are <a href="https://photos.google.com/album/AF1QipMH7TU09SdCnFJVnUSU03R5djZ0XUy_WJTzKOHU" >here</a>
+ *
+ * \section Introduction
+ * Sometimes figuring out how a robot works is hard, so we made this nice documentation.
+ *
+ *
+ */
